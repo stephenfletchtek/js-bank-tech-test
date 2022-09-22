2b61821d4dfddbe3c512aa3a2673bbab961c53e1
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-22 23:39:59 +0100

    refactor

--- statement.js
+++ statement.js
@@ -2,25 +2,25 @@ class Statement {
   createStatement(bankAccount) {
     const outputList = this.#formatTransactionList(bankAccount)
     outputList.push('date || credit || debit || balance')
-    const result = outputList.reverse().join("\n")
-    return result
+    return outputList.reverse().join("\n")
   }
 
-  #twoDecPl(num) {
-    return num.toFixed(2)
+  #formatDate(date) {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
   }
 
   #creditOrDebit(amount) {
     if (amount > 0) {
-      return ` || ${this.#twoDecPl(amount)} || || `
+      return ` || ${amount.toFixed(2)} || || `
     } else {
-      return ` || || ${this.#twoDecPl(-amount)} || `
+      return ` || || ${-amount.toFixed(2)} || `
     }
   }
 
   #formatTransactionList(bankAccount) {
-    const result = bankAccount.allTransactions().map(line => `${line[0].getDate()}/${line[0].getMonth() + 1}/${line[0].getFullYear()}${this.#creditOrDebit(line[1])}${this.#twoDecPl(line[2])}`)
-    return result
+    return bankAccount.allTransactions().map(line =>
+      `${this.#formatDate(line[0])}${this.#creditOrDebit(line[1])}${line[2].toFixed(2)}`
+    )
   }
 }
 
