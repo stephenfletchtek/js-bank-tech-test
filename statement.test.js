d452ef08ff6eae0d2d7b18a9ef5e705900dd4c35
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-23 11:00:28 +0100

    refactor statement.test.js

--- statement.test.js
+++ statement.test.js
@@ -1,53 +1,48 @@
 const Statement = require('./statement')
 
 describe('Statement class', () => {
+  beforeEach(() => {
+    this.statement = new Statement;
+    this.mockBankAccount = {};
+    this.header = 'date || credit || debit || balance';
+  })
+
   it('no transactions', () => {
-    const statement = new Statement;
-    const mockBankAccount = { allTransactions: () => [] }
-    const result = statement.createStatement(mockBankAccount)
-    expect(result).toEqual('date || credit || debit || balance')
+    this.mockBankAccount.allTransactions = () => []
+    const result = this.statement.createStatement(this.mockBankAccount)
+    expect(result).toEqual(this.header)
   })
 
   it('single credit of 200', () => {
-    const statement = new Statement;
-    const mockBankAccount = {}
-    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 200, 200]]
-    const result = statement.createStatement(mockBankAccount)
-    expect(result).toEqual("date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00")
+    this.mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 200, 200]]
+    const result = this.statement.createStatement(this.mockBankAccount)
+    expect(result).toEqual(this.header + "\n20/09/2022 || 200.00 || || 200.00")
   })
 
   it('single credit of 100', () => {
-    const statement = new Statement;
-    const mockBankAccount = {}
-    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 100, 100]]
-    const result = statement.createStatement(mockBankAccount)
-    expect(result).toEqual("date || credit || debit || balance\n20/09/2022 || 100.00 || || 100.00")
+    this.mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 100, 100]]
+    const result = this.statement.createStatement(this.mockBankAccount)
+    expect(result).toEqual(this.header + "\n20/09/2022 || 100.00 || || 100.00")
   })
 
   it('single credit of 3.1415927', () => {
-    const statement = new Statement;
-    const mockBankAccount = {}
-    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 3.1415927, 3.1415927]]
-    const result = statement.createStatement(mockBankAccount)
-    expect(result).toEqual("date || credit || debit || balance\n20/09/2022 || 3.14 || || 3.14")
+    this.mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 3.1415927, 3.1415927]]
+    const result = this.statement.createStatement(this.mockBankAccount)
+    expect(result).toEqual(this.header + "\n20/09/2022 || 3.14 || || 3.14")
   })
 
   it('withdrawal of 50', () => {
-    const statement = new Statement;
-    const mockBankAccount = {}
-    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), -50, -50]]
-    const result = statement.createStatement(mockBankAccount)
-    expect(result).toEqual("date || credit || debit || balance\n20/09/2022 || || 50.00 || -50.00")
+    this.mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), -50, -50]]
+    const result = this.statement.createStatement(this.mockBankAccount)
+    expect(result).toEqual(this.header + "\n20/09/2022 || || 50.00 || -50.00")
   })
 
   it('two transactions', () => {
-    const statement = new Statement;
-    const mockBankAccount = {}
-    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 200, 200], [new Date('2022-11-01'), -50, 150]]
-    const result = statement.createStatement(mockBankAccount)
-    const header = "date || credit || debit || balance\n"
-    const line1 = "01/11/2022 || || 50.00 || 150.00\n"
-    const line2 = '20/09/2022 || 200.00 || || 200.00'
-    expect(result).toEqual(header + line1 + line2)
+    this.mockBankAccount.allTransactions = () =>
+      [[new Date('2022-09-20'), 200, 200], [new Date('2022-11-01'), -50, 150]]
+    const result = this.statement.createStatement(this.mockBankAccount)
+    const line1 = "\n01/11/2022 || || 50.00 || 150.00"
+    const line2 = "\n20/09/2022 || 200.00 || || 200.00"
+    expect(result).toEqual(this.header + line1 + line2)
   })
 })
