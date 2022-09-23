7a286bd8f3711c93e2a641545f206f654a9c38d3
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-23 10:45:37 +0100

    6th statement test passed

--- statement.test.js
+++ statement.test.js
@@ -39,4 +39,15 @@ describe('Statement class', () => {
     const result = statement.createStatement(mockBankAccount)
     expect(result).toEqual("date || credit || debit || balance\n20/09/2022 || || 50.00 || -50.00")
   })
+
+  it('two transactions', () => {
+    const statement = new Statement;
+    const mockBankAccount = {}
+    mockBankAccount.allTransactions = () => [[new Date('2022-09-20'), 200, 200], [new Date('2022-11-01'), -50, 150]]
+    const result = statement.createStatement(mockBankAccount)
+    const header = "date || credit || debit || balance\n"
+    const line1 = "01/11/2022 || || 50.00 || 150.00\n"
+    const line2 = '20/09/2022 || 200.00 || || 200.00'
+    expect(result).toEqual(header + line1 + line2)
+  })
 })
