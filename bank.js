7666aa45eb569fbafeb9c0f03c993c643ea880f1
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-24 15:09:01 +0100

    3 Bank tests passed

---
+++ bank.js
@@ -0,0 +1,31 @@
+const BankAccount = require('./bankAccount')
+const Statement = require('./statement')
+
+class Bank {
+  constructor(bankAccount = new BankAccount, statement = new Statement) {
+    this.bankAccount = bankAccount
+    this.statement = statement
+  }
+
+  showStatement() {
+    return this.statement.createStatement(this.bankAccount)
+  }
+
+  deposit(amount) {
+    this.#checkArgs(amount)
+    return this.bankAccount.transact(amount)
+  }
+
+  withdraw(amount) {
+    this.#checkArgs(amount)
+    this.bankAccount.transact(-amount)
+  }
+
+  #checkArgs(arg) {
+    if (isNaN(arg)) {
+      throw 'method takes a positive number as an argument'
+    }
+  }
+}
+
+module.exports = Bank;
---
+++ bank.test.js
@@ -0,0 +1,33 @@
+const Bank = require('./bank')
+
+describe('Bank class', () => {
+  beforeEach(() => {
+    this.mockBankAccount = {};
+    this.mockStatement = {};
+    this.bank = new Bank(this.mockBankAccount, this.mockStatement);
+    this.header = 'date || credit || debit || balance';
+  })
+
+  it('shows statement with single credit of 200', () => {
+    const line1 = "\n20/09/2022 || 200.00 || || 200.00"
+    this.mockBankAccount.test = true
+    this.mockStatement.createStatement = (arg) => {
+      if (arg.test == true) {
+        return (this.header + line1);
+      }
+    }
+    expect(this.bank.showStatement()).toEqual(this.header + line1);
+  })
+
+  it('deposit 200 into bank', () => {
+    const date = new Date('2022-09-19')
+    this.mockBankAccount.transact = (arg) => [date, arg]
+    expect(this.bank.deposit(200)).toEqual([date, 200])
+  })
+
+  it('wrong number of arguments given', () => {
+    expect(() => {
+      this.bank.deposit();
+    }).toThrow('method takes a positive number as an argument');
+  });
+})
